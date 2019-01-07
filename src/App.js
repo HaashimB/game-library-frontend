eee4cf9a7e0f3ab446cd76ba0d519a72fa2b1196
hashdoge <barihaashim@gmail.com>
2019-01-07 22:54:01 +0000

    Added modal that adds new game, integrates with API from GameLibrary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, Button } from 'antd';
+import { Card, Button, Input } from 'antd';
 import 'antd/lib/card/style/css';
-import 'antd/lib/button/style/css'
+import 'antd/lib/button/style/css';
+import 'antd/lib/input/style/css';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,14 @@ class App extends Component {
         super(props);
         this.state = {
             games: null,
+            addGame: false,
+            newGameTitle: '',
+            newGameDescription: '',
         };
+        this.toggleAddGame = this.toggleAddGame.bind(this);
+        this.submitNewGame = this.submitNewGame.bind(this);
+        this.handleNewGameTitle = this.handleNewGameTitle.bind(this);
+        this.handleNewGameDescription = this.handleNewGameDescription.bind(this);
     }
     componentDidMount() {
         fetch("http://localhost:8080/game/all")
@@ -21,21 +29,50 @@ class App extends Component {
                 });
             });
     }
+
+    toggleAddGame(){
+        this.setState({addGame: !this.state.addGame})
+    }
+    handleNewGameTitle(e){
+        this.setState({newGameTitle: e.target.value})
+    }
+
+    handleNewGameDescription(e){
+        this.setState({newGameDescription: e.target.value})
+    }
+
+    submitNewGame(){
+        fetch("http://localhost:8080/game/add?title=" + encodeURI(this.state.newGameTitle) + "&description=" + encodeURI(this.state.newGameDescription));
+
+    }
     render() {
-        const { games } = this.state;
+        const { games, addGame, newGameTitle, newGameDescription } = this.state;
+        let showAddGame = "show-add-game-1";
         if (!games) {
             return null;
+        }
+        if(addGame) {
+            showAddGame += " show-add-game-2";
         }
         return (
             <div>
                 <div className="game">
                     {games.map(game => {
                         return(
-                                <Card className="card" title={game.title}>{game.description}</Card>
+                            <Card className="display-game-card" title={game.title}>{game.description}</Card>
                         )
                     })}
                 </div>
-                <Button className="add-game" type="primary" htmlType="button">Add New Game</Button>
+                <Button className="add-game" type="primary" htmlType="button" onClick={this.toggleAddGame}>Add New Game</Button>
+                <div className={showAddGame}>
+                    <Card className="add-game-card" title="Add New Game" extra={<a className="close-button" onClick={this.toggleAddGame}>X</a>}>
+                        <Input placeholder="Game Title" value={newGameTitle} onChange={this.handleNewGameTitle} />
+                        <div className="add-game-card-space"> </div>
+                        <Input.TextArea placeholder="Game Description" rows={4} value={newGameDescription} onChange={this.handleNewGameDescription}/>
+                        <div className="add-game-card-space"> </div>
+                        <Button htmlType="button" onClick={this.submitNewGame}> Submit </Button>
+                    </Card>
+                </div>
             </div>
         );
     }
