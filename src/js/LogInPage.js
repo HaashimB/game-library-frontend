204e4ee366f3fc905700ded147fececbf86d2bea
hashdoge <barihaashim@gmail.com>
2019-01-09 14:06:32 +0000

    working request that accesses login endpoint correctly

--- src/js/LogInPage.js
+++ src/js/LogInPage.js
@@ -25,7 +25,15 @@ class LogInPage extends Component {
     }
 
     logIn() {
-        console.log(this.state.username + this.state.password)
+        fetch("http://localhost:8080/user/login?username=" + this.state.username + "&password=" + this.state.password)
+            .then(response => response.text())
+            .then(text => {
+               if(text==="Success"){
+                   console.log("success")
+               }else{
+                   console.log("Wrong username/password");
+               }
+            });
     }
 
     render() {
