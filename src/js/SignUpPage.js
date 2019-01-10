adc47dbbd798da1475dedb40ed49dc116c162e22
hashdoge <barihaashim@gmail.com>
2019-01-10 23:14:18 +0000

    created new class for the sign up page

---
+++ src/js/SignUpPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card, Input, Button } from 'antd';
+import 'antd/lib/card/style/css'
+import 'antd/lib/input/style/css'
+import '../css/LogInPage.css'
+
+class SignUpPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default SignUpPage;
